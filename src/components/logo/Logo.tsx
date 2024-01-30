4159c24cca7a76404ae18523bbf24aeff686e898
Marcin Skic <marcinskicoficjal@gmail.com>
2024-01-30 12:02:10 +0100

    🖌️ Create logo and update favicon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "./logo.module.scss";
+import Image from "next/image";
+import logo from "@/../public/logo.png";
 
 export default function Logo() {
-    return <h1 className={styles.logo}>Name that tune!</h1>;
+    return (
+        <h1 className={styles.logo}>
+            <Image alt="logo" src={logo} height={90} />
+            <span>Name that tune!</span>
+        </h1>
+    );
 }
