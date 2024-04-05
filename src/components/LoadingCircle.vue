2c191210e2ad49750c3a04f2f637f9776f0c6317
MarcinSkic <marcinskicoficjal@gmail.com>
2024-04-05 16:52:56 +0200

    ✨ Migrate LoadingCircle component

---
+++ src/components/LoadingCircle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{ size: string }>()
+</script>
+
+<template>
+  <div
+    class="animation size-[var(--size)] rounded-full border-[calc(var(--size)_*_0.2)] border-solid border-slate-100 border-t-sky-500"
+    :style="`--size:${props.size}`"
+  />
+</template>
+
+<style scoped>
+.animation {
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
