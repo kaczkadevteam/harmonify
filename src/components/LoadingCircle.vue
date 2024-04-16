e6747336cee5ffa35357595a06f87d9b3a1aee78
MarcinSkic <marcinskicoficjal@gmail.com>
2024-04-16 11:55:31 +0200

    ♻️ Remove unnecessary use of props variable in template

--- src/components/LoadingCircle.vue
+++ src/components/LoadingCircle.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const props = defineProps<{ size: string }>()
+defineProps<{ size: string }>()
 </script>
 
 <template>
   <div
     class="animation size-[var(--size)] rounded-full border-[calc(var(--size)_*_0.2)] border-solid border-slate-100 border-t-sky-500"
-    :style="`--size:${props.size}`"
+    :style="`--size:${size}`"
   />
 </template>
 
