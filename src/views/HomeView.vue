999c7df0385197aeb6ba76b6c75e4a34936954de
MarcinSkic <marcinskicoficjal@gmail.com>
2024-05-06 19:04:47 +0200

    feat: prepare UI for connecting with WebSocket

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
 import { useCookies } from '@vueuse/integrations/useCookies'
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
+import { z } from 'zod'
+import { Input } from '@/components/ui/input'
+import { PinInput, PinInputGroup, PinInputInput } from '@/components/ui/pin-input'
+import { Button } from '@/components/ui/button'
 
 const cookies = useCookies()
 
 const isLogged = ref(!!cookies.get('access_token') || !!cookies.get('refresh_token'))
+const roomId = ref<string[]>([])
+const username = ref('')
+const usernameError = ref('')
+
+function joinRoom() {
+  alert(`Joined room ${roomId.value.join('')}`)
+}
+
+function createRoom() {
+  const parseResult = z.string()
+    .min(2, { message: 'Username must contain at least 2 characters' })
+    .max(50, { message: 'Username must contain at most 50 characters' })
+    .safeParse(username.value)
+  if (!parseResult.success) {
+    usernameError.value = parseResult.error.issues[0].message
+    return
+  }
+
+  alert(`Created room`)
+}
+
+function connectToSpotify() {
+  window.location.href = '/api/token/request'
+}
 </script>
 
 <template>
@@ -15,16 +42,43 @@ const isLogged = ref(!!cookies.get('access_token') || !!cookies.get('refresh_tok
         <img src="@/assets/logo.png" alt="Logo" width="100">
         <span>Harmonify!</span>
       </h1>
-      <h3 class="flex items-center gap-3">
-        <span class="text-lg font-semibold">Guess your favorite songs from </span>
-        <img alt="Spotify logo" src="@/assets/Spotify_Logo_RGB_White.png" width="150">
-      </h3>
     </header>
-    <main class="mt-10 text-2xl font-bold text-primary hover:animate-increase-decrease">
-      <RouterLink v-if="isLogged" to="/game">
-        Play
-      </RouterLink>
-      <a v-else href="/api/token/request">Connect</a>
+    <main class="mt-10 text-2xl font-bold">
+      <div class="flex items-end gap-10">
+        <form class="grid justify-items-center gap-5" @submit.prevent="joinRoom">
+          <PinInput
+            id="room-id"
+            v-model="roomId"
+            placeholder="○"
+            @complete="joinRoom"
+          >
+            <PinInputGroup>
+              <PinInputInput v-for="(id, index) in 5" :key="id" :index="index" required />
+            </PinInputGroup>
+          </PinInput>
+
+          <Button>Join room</Button>
+        </form>
+        <form v-if="isLogged" class="grid justify-items-center gap-5" @submit.prevent="createRoom">
+          <p v-if="usernameError" class="-mb-3 text-base font-normal text-destructive">
+            {{ usernameError }}
+          </p>
+          <Input v-model="username" type="text" placeholder="Player name" required @focus="usernameError = ''" />
+          <Button type="submit">
+            Create room
+          </Button>
+        </form>
+        <div v-else class="grid justify-items-center gap-4">
+          <h3 class="flex w-56 flex-wrap items-center gap-x-3">
+            <span class="text-lg font-semibold">Connect to</span>
+            <img alt="Spotify logo" src="@/assets/Spotify_Logo_RGB_White.png" width="100">
+            <span class="text-lg font-semibold">in order to create room</span>
+          </h3>
+          <Button @click="connectToSpotify">
+            Connect
+          </Button>
+        </div>
+      </div>
     </main>
   </div>
 </template>
