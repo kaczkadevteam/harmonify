b5619e2ad6c2f2c732497e17f330817fa5960e7f
MarcinSkic <marcinskicoficjal@gmail.com>
2024-05-13 20:14:03 +0200

    fix: fix host not playing after guess

--- src/components/round/PlaybackControls.vue
+++ src/components/round/PlaybackControls.vue
@@ -1,53 +1,11 @@
 <script setup lang="ts">
-import { onUnmounted, ref, watch } from 'vue'
 import { Pause, Play } from 'lucide-vue-next'
 import VolumeInput from './VolumeInput.vue'
-import { useGameDataStore, useMusicPlayerStore } from '@/stores'
-import type { MusicPlayData, Track } from '@/types'
 import { Button } from '@/components/ui/button'
 
-const props = defineProps<{
+defineProps<{
   isPlaying: boolean
-  musicPlayData: MusicPlayData
 }>()
-
-const isPlayingStarted = ref(false)
-const musicPlayerStore = useMusicPlayerStore()
-const gameDataStore = useGameDataStore()
-
-async function startPlaying() {
-  if (!gameDataStore.selfPlayer.isHost)
-    return
-  if (!isPlayingStarted.value) {
-    isPlayingStarted.value = true
-
-    await musicPlayerStore.play(props.musicPlayData)
-  }
-  else {
-    await musicPlayerStore.resume()
-  }
-}
-
-async function stopPlaying() {
-  if (!gameDataStore.selfPlayer.isHost)
-    return
-  await musicPlayerStore.pause()
-}
-
-watch(() => props.isPlaying, (isPlaying) => {
-  if (isPlaying) {
-    musicPlayerStore.seek(props.musicPlayData.trackStart_ms)
-    startPlaying()
-  }
-
-  else {
-    stopPlaying()
-  }
-})
-
-onUnmounted(() => {
-  stopPlaying()
-})
 </script>
 
 <template>
