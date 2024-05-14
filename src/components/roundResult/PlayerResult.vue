f4e31e0e659fc2203b714e54dd1f6cafcee19ae3
MarcinSkic <marcinskicoficjal@gmail.com>
2024-05-14 18:48:15 +0200

    feat: create component displaying playerResults

---
+++ src/components/roundResult/PlayerResult.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { TransitionPresets, useTransition } from '@vueuse/core'
+import type { PlayerDto } from '@/types'
+
+const props = defineProps<{
+  playerResult: PlayerDto & { width: number }
+}>()
+
+const width = useTransition(() => props.playerResult.width, {
+  duration: 1000,
+  transition: TransitionPresets.linear,
+})
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <div class="w-40 truncate">
+      {{ playerResult.guid }}
+    </div>
+    <div class="h-full rounded-md bg-primary text-right text-primary-foreground" :style="{ width: `${width}rem` }" />
+    <div>{{ playerResult.score }}</div>
+  </div>
+</template>
---
+++ src/components/roundResult/PlayerResults.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useTimeout } from '@vueuse/core'
+import PlayerResult from './PlayerResult.vue'
+import { useResultStore } from '@/stores'
+import type { PlayerDto } from '@/types'
+
+const props = defineProps<{
+  /**
+   * Value in rem
+   */
+  pointsBarMaxWidth: number
+}>()
+
+const resultStore = useResultStore()
+
+const showCurrentScore = useTimeout(1000)
+const playerResults = computed(() => {
+  let results: PlayerDto[] = []
+  if (showCurrentScore.value)
+    results = resultStore.round.players
+  else
+    results = resultStore.round.previousPlayerScores
+
+  results.sort((a, b) => b.score - a.score)
+
+  const bestScore: number = results[0]?.score ?? 0
+
+  return results.map(r => ({ ...r, width: (r.score / bestScore) * props.pointsBarMaxWidth }))
+})
+</script>
+
+<template>
+  <TransitionGroup name="results" tag="div" class="grid gap-4">
+    <PlayerResult v-for="playerResult in playerResults" :key="playerResult.guid" :player-result />
+  </TransitionGroup>
+</template>
+
+<style scoped>
+.results-move,
+.results-enter-active,
+.results-leave-active {
+  transition: all 1s ease;
+}
+
+.results-enter-from,
+.results-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.results-leave-active {
+  position: absolute;
+}
+</style>
