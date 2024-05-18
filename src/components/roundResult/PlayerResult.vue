2d8dbd6269e02867a4b8448a57b14ef15767b047
MarcinSkic <marcinskicoficjal@gmail.com>
2024-05-18 21:33:04 +0200

    feat: add icon for displaying player guess level given round

--- src/components/roundResult/PlayerResult.vue
+++ src/components/roundResult/PlayerResult.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { TransitionPresets, useTransition } from '@vueuse/core'
+import { TransitionPresets, toRefs, useTransition } from '@vueuse/core'
+import { CircleCheck, CircleMinus, CircleX } from 'lucide-vue-next'
+import { computed } from 'vue'
 import type { PlayerScoreDto } from '@/types'
 import { useGameDataStore } from '@/stores'
 import Player from '@/components/Player.vue'
@@ -8,6 +10,7 @@ import { cn } from '@/lib/utils'
 const props = defineProps<{
   animated?: boolean
   playerResult: PlayerScoreDto & { width: number }
+  guessLevel?: 'full' | 'partial' | 'none'
 }>()
 
 const gameDataStore = useGameDataStore()
@@ -15,10 +18,21 @@ const width = useTransition(() => props.playerResult.width, {
   duration: 1000,
   transition: TransitionPresets.linear,
 })
+const [icon, color] = toRefs(computed(() => {
+  switch (props.guessLevel) {
+    case 'full':
+      return [CircleCheck, 'text-green-500']
+    case 'partial':
+      return [CircleMinus, 'text-yellow-500']
+    default:
+      return [CircleX, 'text-red-500']
+  }
+}))
 </script>
 
 <template>
-  <div class="flex items-center gap-2">
+  <div class="relative flex items-center gap-2">
+    <component :is="icon" v-if="guessLevel" :class="cn(' min-w-4 min-h-4 w-4 h-4 self-end -mr-2 relative -bottom-2', color)" />
     <Player
       :player="playerResult"
       :is-self="playerResult.guid === gameDataStore.selfPlayer.guid"
