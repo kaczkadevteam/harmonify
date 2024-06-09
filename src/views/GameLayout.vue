0242f8d56d73e283c49c86e87625fd2b74579173
MarcinSkic <marcinskicoficjal@gmail.com>
2024-06-09 11:13:29 +0200

    feat: add pause button

--- src/views/GameLayout.vue
+++ src/views/GameLayout.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import PauseGameButton from '@/components/PauseGameButton.vue'
 import PreviewPlayer from '@/components/PreviewPlayer.vue'
+import { useGameDataStore } from '@/stores'
+
+const gameDataStore = useGameDataStore()
+const route = useRoute()
+
+const displayPauseButton = computed(() => gameDataStore.selfPlayer.isHost && (route.name === 'round' || route.name === 'roundResult'))
 </script>
 
 <template>
   <div class="grid min-h-screen place-content-center">
+    <PauseGameButton v-if="displayPauseButton" class="fixed right-2 top-2 z-20" />
     <RouterView />
     <PreviewPlayer />
   </div>
