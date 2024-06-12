1cfc2a1516de8f67d480a3a04ac2b65d799a6f49
MarcinSkic <marcinskicoficjal@gmail.com>
2024-06-12 18:25:24 +0200

    feat: add alert dialog when clicking quit during game

--- src/components/QuitGameButton.vue
+++ src/components/QuitGameButton.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { X } from 'lucide-vue-next'
+import { ref } from 'vue'
 import { useConnectionStore } from '@/stores'
 import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 
 const router = useRouter()
 const connectionStore = useConnectionStore()
+const isAlertOpen = ref(false)
 
 function handleClick() {
+  if (router.currentRoute.value.name === 'setup' || router.currentRoute.value.name === 'result')
+    quitGame()
+  else
+    isAlertOpen.value = true
+}
+
+function quitGame() {
   if (connectionStore.ws)
     connectionStore.sendMessage({ $type: 'message', type: 'quitGame' })
   else
@@ -19,4 +39,17 @@ function handleClick() {
   <Button variant="ghost" size="icon" @click="handleClick">
     <X />
   </Button>
+  <AlertDialog v-model:open="isAlertOpen">
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you sure you want to quit game early?</AlertDialogTitle>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction variant="destructive" @click="quitGame">
+          Quit
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
 </template>
