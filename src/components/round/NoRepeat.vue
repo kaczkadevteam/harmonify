3bba1de17bfacf70c7eee97abf3ed45b94ec8903
MarcinSkic <marcinskicoficjal@gmail.com>
2024-06-13 10:52:51 +0200

    feat: create NoRepeat icon

---
+++ src/components/round/NoRepeat.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Repeat, Slash } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="grid grid-cols-1 grid-rows-1 place-content-stretch place-items-stretch">
+    <Repeat class="col-start-1 col-end-2 row-start-1 row-end-2 size-full" />
+    <Slash class="col-start-1 col-end-2 row-start-1 row-end-2 size-full rotate-90 p-0.5" />
+  </div>
+</template>
