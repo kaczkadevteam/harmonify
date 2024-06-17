fb600cced9d7bbcb7108e58bbe53062e81bb9dff
MarcinSkic <marcinskicoficjal@gmail.com>
2024-06-18 00:48:53 +0200

    feat: display incorrect guesses of other players in roundResult

--- src/components/roundResult/PlayerResult.vue
+++ src/components/roundResult/PlayerResult.vue
@@ -7,6 +7,7 @@ import { useGameDataStore } from '@/stores'
 import { cn } from '@/lib/utils'
 import GuessLevelIcon from '@/components/GuessLevelIcon.vue'
 import { AnimationDuration } from '@/consts'
+import { GuessDisplay } from '@/components/trackDisplay'
 
 const props = defineProps<{
   animation?: false | { duration: string }
@@ -19,21 +20,29 @@ const width = useTransition(() => props.playerResult.width, {
   duration: AnimationDuration.D1000,
   transition: TransitionPresets.linear,
 })
+const guess = computed(() => props.playerResult.roundResults.at(-1)!.guess)
 const guessLevel = computed(() => props.playerResult.roundResults.at(-1)!.guessLevel)
 const isSelf = computed(() => props.playerResult.guid === gameDataStore.selfPlayer.guid)
 </script>
 
 <template>
-  <div class="relative flex items-center gap-2">
-    <GuessLevelIcon v-if="displayGuessLevel" :guess-level="guessLevel" class="relative -bottom-2 -mr-2 self-end" />
-    <CircleUserRound :class="cn('min-w-8 min-h-8', isSelf && 'text-primary')" />
-    <div class="grid grid-rows-2">
-      <div class="mr-3 text-sm">
-        {{ props.playerResult.nickname }}
+  <div class="grid">
+    <div class="relative flex items-center gap-2">
+      <GuessLevelIcon v-if="displayGuessLevel" :guess-level="guessLevel" class="relative -bottom-2 -mr-2 self-end" />
+      <div v-else class="min-w-4 min-h-4 relative -bottom-2 -mr-2 self-end"></div>
+      <CircleUserRound :class="cn('min-w-8 min-h-8', isSelf && 'text-primary')" />
+      <div class="grid grid-rows-2">
+        <div class="mr-3 text-sm">
+          {{ props.playerResult.nickname }}
+        </div>
+        <div :class="cn('h-3/4 origin-left rounded-md bg-primary text-right text-primary-foreground', animation && 'animated')" :style="{ width: `${width}px` }" />
       </div>
-      <div :class="cn('h-3/4 origin-left rounded-md bg-primary text-right text-primary-foreground', animation && 'animated')" :style="{ width: `${width}px` }" />
+      <div>{{ playerResult.score }}</div>
     </div>
-    <div>{{ playerResult.score }}</div>
+    <div v-if="displayGuessLevel && guessLevel !== 'full' && guess !== ''" class="text-sm text-nowrap">
+      <span>Guess: </span>
+      <GuessDisplay :guess="guess" />
+    </div>
   </div>
 </template>
 
