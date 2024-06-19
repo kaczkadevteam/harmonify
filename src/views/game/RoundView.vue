057dc4a6a140a5aae57b1549500f59f5411f3cf8
MarcinSkic <marcinskicoficjal@gmail.com>
2024-06-19 14:24:39 +0200

    feat: display round number and time left when waiting after guess

--- src/views/game/RoundView.vue
+++ src/views/game/RoundView.vue
@@ -62,27 +62,29 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="!isGuessSubmitted" class="grid grid-cols-2 place-content-center place-items-center gap-x-40 gap-y-10 self-start p-4 md:mb-60 md:mt-4 md:self-center md:justify-self-center md:p-0">
+  <div class="grid grid-cols-2 place-content-center place-items-center gap-x-40 gap-y-10 self-start p-4 md:mb-60 md:mt-4 md:self-center md:justify-self-center md:p-0">
     <span class=" justify-self-start text-xl">Round: {{ gameDataStore.round }}</span>
 
     <div class=" flex items-center gap-6 justify-self-end">
       <CircularTimer :x="roundTimeLeft" :x-max="gameDataStore.gameSettings.roundDuration" />
     </div>
-    <PlaybackControls
-      class="col-span-2 mt-20"
-      :music-play-data="gameDataStore.musicPlayData"
-    />
-    <form class="col-span-2 grid grid-cols-2 place-items-center gap-y-4" @submit.prevent="handleGuessSubmit">
-      <SearchInput v-model="guess" :guesses="gameDataStore.possibleGuesses" class=" col-span-2" />
-      <Button type="submit" value="submit">
-        Submit
-      </Button>
-      <Button type="submit" value="skip" variant="destructive" class="col-start-1 row-start-2">
-        Skip
-      </Button>
-    </form>
-  </div>
-  <div v-else class="place-self-center">
-    Waiting for other players to submit guesses
+    <template v-if="!isGuessSubmitted">
+      <PlaybackControls
+        class="col-span-2 mt-20"
+        :music-play-data="gameDataStore.musicPlayData"
+      />
+      <form class="col-span-2 grid grid-cols-2 place-items-center gap-y-4" @submit.prevent="handleGuessSubmit">
+        <SearchInput v-model="guess" :guesses="gameDataStore.possibleGuesses" class=" col-span-2" />
+        <Button type="submit" value="submit">
+          Submit
+        </Button>
+        <Button type="submit" value="skip" variant="destructive" class="col-start-1 row-start-2">
+          Skip
+        </Button>
+      </form>
+    </template>
+    <div v-else class="col-span-2 mt-40">
+      <span>Waiting for other players to submit guesses</span>
+    </div>
   </div>
 </template>
