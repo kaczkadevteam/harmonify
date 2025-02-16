7ef08c312bf17f9644ed1bd1930e4c0757e5a339
MarcinSkic <marcinskicoficjal@gmail.com>
2025-02-16 09:44:39 +0100

    feat: add scrollarea to covers list

--- src/views/CoverCreatorView.vue
+++ src/views/CoverCreatorView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { ChevronLeft, ChevronRight } from 'lucide-vue-next'
-import { computed, ref } from 'vue'
-import type { COVERS_KEYS } from '@/consts'
+import { ref } from 'vue'
 import { COVERS } from '@/consts'
 import Cover from '@/components/coverCreator/Cover.vue'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
 const size = 800
 const cssSize = `${size}px`
@@ -39,25 +38,30 @@ function setCover(index: number) {
 </script>
 
 <template>
-  <div class="flex gap-2">
-    <Cover
-      v-for="cover, idx of covers"
-      :key="idx"
-      :size="100"
-      :base-color="cover.color"
-      :title="cover.title"
-      :subtitle="cover.subtitle"
-      :example="cover.example"
-      @click="setCover(idx)"
-    />
-  </div>
-  <div :class="`grid w-[${cssSize}] *:col-start-1 *:row-start-1`">
-    <Cover
-      :size
-      :base-color="currentCover.color"
-      :title="currentCover.title"
-      :subtitle="currentCover.subtitle"
-      :example="currentCover.example"
-    />
+  <div class="p-4">
+    <ScrollArea class="w-full">
+      <div class="flex gap-2 py-1">
+        <Cover
+          v-for="cover, idx of covers"
+          :key="idx"
+          :size="100"
+          :base-color="cover.color"
+          :title="cover.title"
+          :subtitle="cover.subtitle"
+          :example="cover.example"
+          @click="setCover(idx)"
+        />
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+    <div :class="`grid w-[${cssSize}] *:col-start-1 *:row-start-1`">
+      <Cover
+        :size
+        :base-color="currentCover.color"
+        :title="currentCover.title"
+        :subtitle="currentCover.subtitle"
+        :example="currentCover.example"
+      />
+    </div>
   </div>
 </template>
